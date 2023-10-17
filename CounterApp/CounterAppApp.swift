18e9294e88d3c6c0f8def57bc9843e68f072dc52
kotathefriends <5070kouta@gmail.com>
2023-10-17 22:06:14 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by KOTA TAKAHASHI on 2023/10/17.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
